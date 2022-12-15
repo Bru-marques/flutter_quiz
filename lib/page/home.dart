e50fa38046c9f18fca03e3fcdda290b26e41be6c
Bruna Marques <brmarques1108@gmail.com>
2022-12-15 15:19:06 +0000

    layout

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -6,6 +6,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xffd8cdfc),
       body: _body(),
     );
   }
@@ -13,20 +14,117 @@ class Home extends StatelessWidget {
   _body() {
     return SafeArea(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
         child: Column(
-          children: [
-            Container(
-              height: 140,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: const Color(0xff6158E0),
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [_scoreContainer(), _question(), _buttonGroup()],
+        ),
+      ),
+    );
+  }
+
+  Widget _scoreContainer() {
+    return Container(
+      height: 120,
+      width: double.infinity,
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: const Color(0xff6158E0),
+      ),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 80,
+            width: 80,
+            child: CircularProgressIndicator(
+              value: 0.2,
+              strokeWidth: 8,
+              color: Colors.deepOrangeAccent,
+              backgroundColor: Colors.white.withOpacity(.2),
+            ),
+          ),
+          const SizedBox(width: 30),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Text(
+                'Score',
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              child: Row(
-                children: [],
+              Row(
+                children: [
+                  Icon(Icons.star,
+                      size: 38, color: Colors.white.withOpacity(.2)),
+                  Icon(Icons.star,
+                      size: 38, color: Colors.white.withOpacity(.2)),
+                  Icon(Icons.star,
+                      size: 38, color: Colors.white.withOpacity(.2)),
+                  Icon(Icons.star,
+                      size: 38, color: Colors.white.withOpacity(.2)),
+                  Icon(Icons.star,
+                      size: 38, color: Colors.white.withOpacity(.2)),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _question() {
+    return const Text(
+      textAlign: TextAlign.center,
+      'The blue whale is the biggest animal to have ever lived.',
+      style: TextStyle(
+        fontSize: 28,
+        color: Colors.black87,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget _buttonGroup() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        _button('True', Icons.thumb_up_alt),
+        const SizedBox(height: 12),
+        _button('False', Icons.thumb_down)
+      ],
+    );
+  }
+
+  Widget _button(String title, IconData icon) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xff6158E0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              textAlign: TextAlign.center,
+              title,
+              style: const TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
             ),
+            const SizedBox(
+              width: 8,
+            ),
+            Icon(icon, color: Colors.white.withOpacity(.6)),
           ],
         ),
       ),
